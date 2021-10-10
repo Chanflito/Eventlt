8e6af0f7a69d8a28b2faaf2ae6d6566df0beec18
Chanflito <luciano.gianfelice@ing.austral.edu.ar>
2021-10-10 20:58:21 -0300

    cambios

--- Clases/usuario.py
+++ Clases/usuario.py
@@ -1,10 +1,12 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
 class usuario(ABC):
-    @abstractmethod
     def __init__(self, name, lastName, age, numTel, CUIL):
         self.name = name
         self.lastName = lastName
         self.age = age
         self.numTel = numTel
         self.CUIL = CUIL
+    def __str__(self):
+        return self.name + " " + self.lastName
+        
