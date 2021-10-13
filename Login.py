95450301007e013a665803fd52cff15658fbf482
Chanflito <luciano.gianfelice@ing.austral.edu.ar>
2021-10-12 21:59:27 -0300

    Solucion de bugs

--- Login.py
+++ Login.py
@@ -3,6 +3,15 @@ import os
 import pandas #se debe descargar esta libreria con pip install pandas en la terminal
 import time
 def register():
+    phonenumber=input("Ingrese su numero telefonico: (+54) ")
+    try:
+        int(phonenumber)
+
+    except ValueError:
+        print("Debe ingresar su numero telefonico correctamente")
+        time.sleep(3)
+        os.system("cls")
+        menu()
     user_cuil = input("Ingrese su CUIL: ")
     try:
         int(user_cuil)
@@ -24,11 +33,33 @@ def register():
         counter = len(str(user_cuil))
         password = input("Ingrese su contraseña,porfavor: ")
         password1 = input("Confirme su contraseña,nuevamente: ")
-        if counter == 11 and password == password1:
+        counter2 = len(str(phonenumber))
+        name = input('Ingrese su nombre ')
+        surname = input('Ingrese su apellido ')
+        try:
+            if str(name).isdigit() == True or str(surname).isdigit() == True:
+                raise ValueError()
+
+        except ValueError:
+            print ('Ingresaste numeros en tu nombre o apellido, intentelo denuevo.')
+            time.sleep(3)
+            os.system("cls")
+            menu()
+        try:
+            age =input("dime tu edad: ")
+            int(age)
+        except ValueError:
+            print("Usted ha ingresado un caracter invalido, intentelo nuevamente.")
+            time.sleep(3)
+            os.system("cls")
+            menu()
+            
+        if counter == 11 and password == password1 and counter2==11:
             print("Te has registrado correctamente.")
             with open("Database.csv",mode="a",newline="") as h:
                 writer=csv.writer(h,delimiter=",")
-                writer.writerow([user_cuil,password])
+                writer.writerow([user_cuil,password,phonenumber])
+                #citizen.create_citizen(name, surname, age, user_cuil)
                 print ("Bienvenido")
                 h.close()
         elif counter != 11:
@@ -41,6 +72,14 @@ def register():
             time.sleep(3)
             os.system("cls")
             register()
+
+        elif counter2 != 11:
+            print("Cantidad de digitos de su numero telefonico es incorrecta.")
+            time.sleep(3)
+            os.system("cls")
+            register()
+
+            
 df = pandas.read_csv(r"Database.csv")
 def login():
     user_cuil=(input("Ingrese su CUIL: ")) #arreglar con exepcion si tira con un string
