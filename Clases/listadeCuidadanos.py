59a202b69bf48382ac9736d5a70244530a1f8f7c
Valentino Di Capua <92276538+valendc15@users.noreply.github.com>
2021-10-16 16:33:33 -0300

    Update listadeCuidadanos.py

--- Clases/listadeCuidadanos.py
+++ Clases/listadeCuidadanos.py
@@ -1,8 +1,19 @@
 class CitizenList():
     def __init__ (self):
         self.citizenlist=[]
+        self.BannedCitizenList=[]
     def addCitizen(self, citizen):
         self.citizenlist.append(citizen)
+    def addBannedCitizen(self, citizen):
+        self.BannedCitizenList.append(citizen)
+    def removeBannedCitizen(self, citizen):
+        i=0
+        while i<len(self.BannedCitizenList):
+            if citizen==self.BannedCitizenList[i]:
+                del self.BannedCitizenList[i]
+                return self.BannedCitizenList
+            else:
+                i=i+1
     def getcl(self):
         return(self.citizenlist)
 
