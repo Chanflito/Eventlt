fdd84c796a40aef3c2def1916b243bab4215905b
FranManfredi <85314753+FranManfredi@users.noreply.github.com>
2021-10-18 19:01:47 -0300

    Add files via upload

---
+++ Clases/listadeCuidadanos.py
@@ -0,0 +1,20 @@
+from citizen import ciudadano as creadordeciudadano
+import pandas
+
+class CitizenList():
+
+    def __init__ (self):
+        self.citizenlist=[]
+        df = pandas.read_csv(r"D:\Codigos\TP1Prog\Database.csv")
+        i = 0
+        while i != len(df['CUIL']):
+            self.citizenlist.append(creadordeciudadano.init_citizen_creation(df['name'][i], df['surname'][i], df['age'][i], df['CUIL'][i], df['Phonenumber'][i]))
+            i += 1
+    
+    def addCitizen(self, citizen):
+        self.citizenlist.append(citizen)
+
+    def getcl(self):
+        return(self.citizenlist)
+
+etlist = CitizenList()
