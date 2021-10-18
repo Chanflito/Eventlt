e449f2b408ac35b8740eb67ebb035e52ba0fe938
FranManfredi <85314753+FranManfredi@users.noreply.github.com>
2021-10-18 19:01:38 -0300

    Delete listadeCuidadanos.py

--- Clases/listadeCuidadanos.py
+++
@@ -1,9 +0,0 @@
-class CitizenList():
-    def __init__ (self):
-        self.citizenlist=[]
-    def addCitizen(self, citizen):
-        self.citizenlist.append(citizen)
-    def getcl(self):
-        return(self.citizenlist)
-
-etlist = CitizenList()
