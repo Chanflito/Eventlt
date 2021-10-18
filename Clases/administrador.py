a2b5eb284842857bf3df63468dd9abcfd14b7499
FranManfredi <85314753+FranManfredi@users.noreply.github.com>
2021-10-18 19:03:06 -0300

    Add files via upload

---
+++ Clases/administrador.py
@@ -0,0 +1,13 @@
+class administrator():
+    def __init__(self, user):
+        self.user = user
+        
+
+    def addEvent(self):
+        pass
+
+    def banCitizen(self):
+        pass
+
+    def unbanCitizen(self):
+        pass
