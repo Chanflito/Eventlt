38417574bc112d7657e152cd05defd4b99239eb2
Valentino Di Capua <92276538+valendc15@users.noreply.github.com>
2021-11-16 14:53:05 -0300

    Update administrador.py

--- administrador.py
+++ administrador.py
@@ -2,17 +2,24 @@ from listadeCuidadanos import etlist
 from listaDeEventos import eventos
 
 class administrator():
-  
+    listaderevision=[]
+
     def __init__(self, user):
         self.user = user
+
+    @classmethod
+    def mandar_a_revision(cls,citizen):
+        cls.listaderevision.append(citizen)
 
     def addEvent(self, location, description):
         eventos.eventCreator(location, description)
 
-    def banCitizen(self,citizen):
+    @staticmethod
+    def banCitizen(citizen):
         citizen.citizenBan=True
         etlist.addBannedCitizen(citizen)
         
-    def unbanCitizen(self,citizen):
+    @staticmethod
+    def unbanCitizen(citizen):
         citizen.citizenBan=False
         etlist.removeBannedCitizen(citizen)
