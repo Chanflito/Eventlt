5d491881b6e5e08bb14a4606e4bdc87873661e0d
Valentino Di Capua <92276538+valendc15@users.noreply.github.com>
2021-11-15 22:55:58 -0300

    Update menu_completo.py

--- menu_completo.py
+++ menu_completo.py
@@ -3,34 +3,33 @@ import os
 import pandas #se debe descargar esta libreria con pip install pandas en la terminal
 import time
 from citizen import ciudadano
-from listadeCuidadanos import etlist
 import new_world
 import webbrowser
 import random
+from listadeCuidadanos import etlist
 
-class menuCompleto:
-    @staticmethod
-    def menu_o():
+class MainMenu:
+    def menu_o(self):
         try:
             print ('Bienvenido a Eventlt')
             menu_login=int(input('1.Ingresar como Admin | 2.Ingresar como usuario | 3.Ingresar como sensor | ingresar cualquier otro numero para salir:'))
 
             if menu_login == 1:
-                menuCompleto.log_adm()
+                self.log_adm()
 
             elif menu_login == 2:
-                menuCompleto.menu_login_citizen()
+                self.menu_login_citizen()
 
             elif menu_login == 3:
                 new_world.Mapa.show_map()
-                volver=int(input("1.Volver al menu | 2. Cerrar programa: "))
-                if volver==1:
-                    menuCompleto.menu_o()
+                go_back = int(input("1.Volver al menu | 2. Cerrar programa: "))
+                if go_back ==1 :
+                    self.menu_o()
                 else:
                     os.system('cls' if os.name == 'nt' else 'clear')
 ############################################################################################################## menu secreto autistico
             elif menu_login == 69 or menu_login == 420:
-                randnum = random.randint(1, 8)
+                randnum = random.randint(1, 7)
                 if randnum == 1:
                     webbrowser.open('https://www.youtube.com/watch?v=auVgp3HcYaY')
                 elif randnum == 2:
@@ -47,7 +46,7 @@ class menuCompleto:
                     webbrowser.open('https://www.youtube.com/watch?v=DAlrY0iZKwQ')
                 elif randnum == 8:
                     webbrowser.open('https://www.youtube.com/watch?v=OTwJ97Q6EzI')
-                menuCompleto.menu_o()
+                self.menu_o()
 
 #######################################################################################################################################
             else:
@@ -55,19 +54,18 @@ class menuCompleto:
         except ValueError:
             print ('Para salir del programa debe presionar un numero.')
             time.sleep(3)
-            menuCompleto.menu_o()
-        
-    @staticmethod
-    def register():
-        phonenumber=input("Ingrese su numero telefonico: (+54) ")
+            self.menu_o()
+
+    def register(self):
+        phone_number=input("Ingrese su numero telefonico: (+54) ")
         try:
-            int(phonenumber)
+            int(phone_number)
         except ValueError:
             print("Debe ingresar su numero telefonico correctamente")
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
 
-            menuCompleto.menu_login_citizen()
+            self.menu_login_citizen()
         user_cuil = input("Ingrese su CUIL: ")
         try:
             int(user_cuil)
@@ -75,29 +73,28 @@ class menuCompleto:
             print("El CUIL debe ser un número")
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
-
-            menuCompleto.menu_login_citizen()
+            self.menu_login_citizen()
 
         df = pandas.read_csv(os.path.abspath("Database.csv"))
-        for i in df['Phonenumber']:
-            if str(i) == str(phonenumber):
+        for numbers in df['Phonenumber']:
+            if str(numbers) == str(phone_number):
                 print ("Ya hay un usuario registrado con este numero telefonico")
                 time.sleep(3)
                 os.system('cls' if os.name == 'nt' else 'clear')
 
-                menuCompleto.menu_login_citizen()
+                self.menu_login_citizen()
         
-        for i in df['CUIL']:
-            if str(i) == str(user_cuil):
+        for CUILnumbers in df['CUIL']:
+            if str(CUILnumbers) == str(user_cuil):
                 print ("Ya hay un usuario registrado con este CUIL")
                 time.sleep(3)
                 os.system('cls' if os.name == 'nt' else 'clear')
 
-                menuCompleto.menu_login_citizen()
+                self.menu_login_citizen()
 
         counter = len(str(user_cuil))
 
-        counter2 = len(str(phonenumber))
+        counter2 = len(str(phone_number))
 
         password = input("Ingrese su contraseña,porfavor: ")
 
@@ -115,7 +112,7 @@ class menuCompleto:
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
 
-            menuCompleto.menu_login_citizen()
+            self.menu_login_citizen()
 
         try:
             age =input("cual es tu edad: ")
@@ -127,36 +124,36 @@ class menuCompleto:
         if counter == 11 and password == password1 and counter2==11:
             with open(os.path.abspath("Database.csv"),mode="a",newline="") as h:
                 writer=csv.writer(h,delimiter=",")
-                writer.writerow([user_cuil,password,phonenumber,name,surname,age])
-            ciudadano.create_citizen(name,surname,age, user_cuil,phonenumber)
+                writer.writerow([user_cuil,password,phone_number,name,surname,age])
+            ciudadano.create_citizen(name,surname,age, user_cuil,phone_number)
             print(etlist.getcl())
             # print("Su clase citizen fue creada exitosamente")
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
 
             h.close()
-            menuCompleto.menu_login_citizen()    
+            self.menu_login_citizen()
         elif counter != 11:
             print("Has ingresado el numero incorrecto de un CUIL.")
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
 
-            menuCompleto.register()
+            self.register()
         elif password != password1:
             print("Contraseña Incorrecta, intentelo denuevo.")
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
 
-            menuCompleto.register()
+            self.register()
         elif counter2 != 11:
             print("Cantidad de digitos de su numero telefonico es incorrecta.")
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
 
-            menuCompleto.register()
+            self.register()
 
-    @staticmethod
-    def login():
+
+    def login (self):
         df = pandas.read_csv(os.path.abspath("Database.csv"))
         user_cuil=(input("Ingrese su CUIL: ")) 
         try:
@@ -166,7 +163,7 @@ class menuCompleto:
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
 
-            menuCompleto.menu_login_citizen()
+            self.menu_login_citizen()
         user_cuil=int(user_cuil)
         i = 0
         for a in df['CUIL']:
@@ -178,43 +175,42 @@ class menuCompleto:
                 time.sleep(3)
                 os.system('cls' if os.name == 'nt' else 'clear')
 
-                menuCompleto.menu_login_citizen()
+                self.menu_login_citizen()
                 return i
         password = input("Ingrese su contraseña: ")
         if str(password) == str(df['Password'][i]):
             print("Bienvenido a Eventlt.\n\n")
-            menuCompleto.menu_citizen(i) #<---- esto linkea al menu de las acciones del ciudadano
+            self.menu_citizen(i) #<---- esto linkea al menu de las acciones del ciudadano
         else:
             print("Contraseña invalida.")
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
 
-            menuCompleto.menu_login_citizen()
+            self.menu_login_citizen()
 
-    @staticmethod
-    def menu_login_citizen():
+
+    def menu_login_citizen(self):
         print ("Porfavor, seleccione una de las siguentes opciones")
         try:
             user=int(input("1.Log in | 2. Registrarse | 3.Salir : "))
         except ValueError:
             print('eso no es un numero, intentelo nuevamente')
-            menuCompleto.menu_login_citizen()
+            self.menu_login_citizen()
         if user == 1:
-            menuCompleto.login()
+            self.login()
         elif user==2:
-            menuCompleto.register()
+            self.register()
         elif user==3:
-            menuCompleto.menu_o()
+            self.menu_o()
         else:
             print ("Debes ingresar los numeros indicados anteriormente.")
-            menuCompleto.menu_login_citizen()
-    
-    @staticmethod
-    def menu_citizen(i):
+            self.menu_login_citizen()
+
+    def menu_citizen(self,citizenidentifier):
         df = pandas.read_csv(os.path.abspath("Database.csv"))
         seconddf = pandas.read_csv(os.path.abspath("Eventos.csv"))
         for a in etlist.citizenlist:
-            if int(df['CUIL'][i]) == int(a.CUIL):
+            if int(df['CUIL'][citizenidentifier]) == int(a.CUIL):
                 x = a
         if x.zone == -1:
             b = int(input(f'bienvenido al menu_citizen por primera vez, {x.name}!\n\nPorfavor, ingrese el numero respectivo a su zona:\n{registroDeZonas.listadoZonas(seconddf)}')) # SOLO PUEDE SER UN NUMERO
@@ -222,7 +218,7 @@ class menuCompleto:
                 print('este numero no es valido, vuelva a intentarlo')
                 time.sleep(3)
                 os.system('cls' if os.name == 'nt' else 'clear')
-                menuCompleto.menu_citizen(i)
+                self.menu_citizen(citizenidentifier)
             else:
                 print('muchas gracias!')
                 time.sleep(3)
@@ -234,7 +230,7 @@ class menuCompleto:
             print ('Debe ingresar un numero')
             time.sleep(3)
             os.system('cls' if os.name== 'nt' else 'clear')
-            menuCompleto.menu_citizen(i)
+            self.menu_citizen(citizenidentifier)
         if c == 1:
             pass
         elif c == 2:
@@ -244,39 +240,40 @@ class menuCompleto:
         elif c == 4:
             pass
         
-    @staticmethod
-    def log_adm():
+
+    def log_adm (self):
         df = pandas.read_csv(os.path.abspath("Base_Adm.csv")) #va el path csv de administradores
         admin_user=input("Ingrese su nombre de usuario como admnistrador: ")
-        i = 0
+        admin_serch = 0
         for a in df['Admin_user']:
             if str(admin_user) == str(a):
                 break
-            i += 1
-        if i == len(df["Admin_user"]):
+            admin_serch += 1
+        if admin_serch == len(df["Admin_user"]):
             print('su nombre no esta como usuario, volviendo al menu principal.')
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
-            menuCompleto.menu_o()
-            return i
+            self.menu_o()
+            return admin_serch
         admin_password=input('Ingrese su contraseña: ')
-        if str(admin_password) == str(df['Password'][i]):
+        if str(admin_password) == str(df['Password'][admin_serch]):
             print(f"Usted ingreso como {admin_user}, bienvenido.")
         else:
             print("Contraseña invalida, volviendo al menu principal.")
             time.sleep(3)
             os.system('cls' if os.name == 'nt' else 'clear')
-            menuCompleto.menu_o()
+            self.menu_o()
 
-    @staticmethod
-    def menu_administrador():
+
+    def menu_administrador(self):
         pass
     
-    @staticmethod
-    def friends_menu():
+
+    def friends_menu(self):
         pass
 
 class registroDeZonas:
+
     @staticmethod
     def listadoZonas(eventos):  
         zonitas = ''
@@ -285,6 +282,3 @@ class registroDeZonas:
             zonitas += f'{i} |' + zona + '\n'
             i += 1
         return zonitas
-
-
-menuCompleto.menu_o()
