d5d2aeb0c97539d7ef80785b6a0c95121e46dd64
EmiLopez23 <92272247+EmiLopez23@users.noreply.github.com>
2021-11-17 12:11:15 -0300

    Update new_world.py

--- new_world.py
+++ new_world.py
@@ -2,22 +2,26 @@ import folium
 import pandas as pd
 import os
 import sys , subprocess
-
+from folium.plugins import MarkerCluster
+mc=MarkerCluster()
+df_marcador = pd.read_csv(os.path.abspath("marcadores.csv"))
 
 class Mapa():
     @staticmethod
     def show_map (lat, lon):
-        marcadores = "marcadores.csv"
         i = 0
-        df_marcador = pd.read_csv(os.path.abspath(marcadores))
         mapita = folium.Map(location=[lat,lon], zoom_start=15, dragging=False)
         for _, marcador in df_marcador.iterrows():
-            folium.Marker(location=[float(marcador["Latitud"]), float(marcador["Longitud"])],
+            mapita.add_child(folium.Marker(location=[float(marcador["Latitud"]), float(marcador["Longitud"])],
             tooltip=(marcador["Nombre"]), 
-            icon=folium.Icon(color='blue'), 
-            popup = folium.Popup(marcador["Descripcion"], min_width=300, max_width=300)).add_to(mapita) 
+            icon=folium.Icon(color='blue'), popup = folium.Popup(marcador["Descripcion"], min_width=300, max_width=300)))
             i += 1
-        mapita.save(os.path.abspath("index.html"))
+        mapita2 = folium.Map(location=[-36.8939975,-60.3227331], zoom_start=7)
+        for _, marcador in df_marcador.iterrows():
+            mc.add_child(folium.Marker(location=[float(marcador["Latitud"]), float(marcador["Longitud"])],
+            tooltip=(marcador["Nombre"]), popup=marcador["Descripcion"]))
+        mapita2.add_child(mc)
+        mapita2.save(os.path.abspath("index.html"))
         
         if os.sys.platform == "win32":
             return os.startfile("index.html") #Open map with Windows System.
